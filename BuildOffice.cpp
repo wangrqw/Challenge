ab16720a4b4669cdfca99163bae3273dda2f0c27
wangrqw <wang.6863@osu.edu>
2020-02-13 16:08:27 -0500

    add BuildOffice problem

---
+++ BuildOffice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+    public: int minDistance(){
+        return 0;
+    }
+};
+int main(){
+    int w, h, n;
+    cin>> w >> h >> n;
+    cout << w << " " << h << " " << n<<"\n";
+    Solution sol;
+    cout << sol.minDistance();
+    return 0;
+}
