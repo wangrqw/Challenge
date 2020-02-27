b1f7d7e2e28c1dacc5d3a18c01923a2f66587387
Rachel Wang <rachel@Rachels-MacBook-Air.local>
2020-02-27 18:22:51 -0500

    runtime O(n)

--- BuyTickets.cpp
+++ BuyTickets.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-class Solution{
+#include <vector>
 
+using namespace std;
+
+class Solution{
+    public: int waitingTime(int size, vector<int> tickets, int pos){
+        int time=0;
+        for(int i=0;i<pos;i++){
+            if(tickets[i]<tickets[pos]) 
+                time += tickets[i];
+            else 
+                time += tickets[pos];
+        }
+        for(int i=pos+1;i<size;i++){
+            if(tickets[i]<tickets[pos]-1) 
+                time += tickets[i];
+            else 
+                time += tickets[pos]-1;
+        }
+        return time;
+    }
 };
 
 int main(){
-    
+
 return 0;
 }
